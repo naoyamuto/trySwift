0d2f1866436a8549c2a5b9901d43a8f45719a399
Helen V. Holmes <helen.v.holmes@gmail.com>
2015-11-04 16:06:44 -0800

    Hook up front end with erb database tags

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @speakers = Speaker.all
+  end
 end
