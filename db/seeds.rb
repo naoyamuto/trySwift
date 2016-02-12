11f63818dfc04ce597502afd1457cf5c79074345
kishikawa katsumi <kishikawakatsumi@mac.com>
2016-02-13 04:27:56 +0900

    Add sponsor caraquri

--- db/seeds.rb
+++ db/seeds.rb
@@ -135,3 +135,4 @@ Sponsor.create(name: "株式会社PR TIMES", website: "http://prtimes.co.jp/", l
 Sponsor.create(name: "株式会社オハコ", website: "http://ohako-inc.jp/", logo: "ohako.png", level: "Silver")
 Sponsor.create(name: "株式会社ookami", website: "http://www.playerapp.tokyo", logo: "ookami.png", level: "Silver")
 Sponsor.create(name: "Perfect", website: "http://perfect.org/", logo: "perfect.png", level: "Silver")
+Sponsor.create(name: "からくり株式会社", website: "http://caraquri.com/", logo: "caraquri.png", level: "Silver")
