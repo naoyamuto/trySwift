fff5d1de5171acfc22087f7a157f47f41cd57075
kishikawa katsumi <kishikawakatsumi@mac.com>
2016-02-14 03:05:56 +0900

    Fix html rendering

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
-class WelcomeController < ActionController::Base
+class WelcomeController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout "application_en", only: [:index_en]
 
   def index_en
     @speakers = Speaker.all
